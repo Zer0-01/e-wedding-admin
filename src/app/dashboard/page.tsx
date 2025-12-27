51f50d3f9191355726bb0f2286bc0fcdaf00aa08
Anas <anas.zulkifli2001@gmail.com>
2025-12-27 09:06:09 +0800

    feat: integrate logout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+
 import AdminGuard from "@/components/admin-guard"
+import { auth } from "@/utils/client-credentials"
+import { signOut } from "firebase/auth"
 
 export default function DashboardPage() {
     return (
         <AdminGuard>
             <div>Admin Dashboard</div>
-        </AdminGuard>
+            <button
+                onClick={() => signOut(auth)}
+                className="text-sm text-muted-foreground"
+            >
+                Logout
+            </button>        </AdminGuard>
     )
 }

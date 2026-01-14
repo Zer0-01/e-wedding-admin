a8c83087ff95716a6980a5477c2c2fd64025adf3
Anas <anas.zulkifli2001@gmail.com>
2026-01-14 23:25:40 +0800

    feat: tanstack form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,62 +6,122 @@ import { useRouter } from "next/navigation"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { useState } from "react"
 import { toast } from "sonner"
+import { useForm } from "@tanstack/react-form"
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
 
 export default function LoginPage() {
+    const form = useForm({
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+
+        onSubmit: ({ value }) => {
+           login(value.email, value.password)
+        }
+    })
+
     const router = useRouter()
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+
     const [loading, setLoading] = useState(false)
 
-    const login = async () => {
+    const login = async (email: string, password: string) => {
         try {
             setLoading(true)
             await signInWithEmailAndPassword(auth, email, password)
             router.replace("/dashboard")
-        } catch  {
+        } catch {
             toast.error("Failed to sign in")
         } finally {
             setLoading(false)
         }
-    }   
+    }
 
     return (
         <div className="min-h-screen flex items-center justify-center px-4">
             <Card className="w-full max-w-sm">
-                <CardHeader className="space-y-1">
-                    <CardTitle className="text-2xl font-semibold tracking-tight">
+                <CardHeader>
+                    <CardTitle>
                         Admin Login
                     </CardTitle>
                 </CardHeader>
 
-                <CardContent className="space-y-4">
-                    <div className="space-y-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            placeholder="admin@email.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
+                <CardContent className="space-y-4" >
+                    <form id="login-form" onSubmit={(e) => {
+                        e.preventDefault()
+                        form.handleSubmit()
+                        
+                    }} className="space-y-4">
+                        <FieldGroup>
+                            <form.Field name="email">
 
-                    <div className="space-y-2">
-                        <Label htmlFor="password">Password</Label>
-                        <Input
-                            id="password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
+                                {(field) => {
+                                    const isInvalid =
+                                        field.state.meta.isTouched && !field.state.meta.isValid
+                                    return (
+                                        <Field data-invalid={isInvalid}>
+                                            <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                                            <Input
+                                                id={field.name}
+                                                name={field.name}
+                                                value={field.state.value}
+                                                onBlur={field.handleBlur}
+                                                onChange={(e) => field.handleChange(e.target.value)}
+                                                aria-invalid={isInvalid}
+                                                placeholder="john_doe@email.com"
+                                            />
+                                        </Field>
+                                    )
+                                }
+
+                                }
+
+
+
+                            </form.Field>
+
+                        </FieldGroup>
+                        <FieldGroup>
+                            <form.Field name="password">
+
+                                {(field) => {
+                                    const isInvalid =
+                                        field.state.meta.isTouched && !field.state.meta.isValid
+                                    return (
+                                        <Field data-invalid={isInvalid}>
+                                            <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                                            <Input
+                                                id={field.name}
+                                                name={field.name}
+                                                value={field.state.value}
+                                                onBlur={field.handleBlur}
+                                                onChange={(e) => field.handleChange(e.target.value)}
+                                                aria-invalid={isInvalid}
+                                                type="password"
+                                                
+                                            />
+                                        </Field>
+                                    )
+                                }
+
+                                }
+
+
+
+                            </form.Field>
+
+                        </FieldGroup>
+
+
+                    </form>
+
 
                     <Button
                         className="w-full"
-                        onClick={login}
+                        form="login-form"
+                        type="submit"
                         disabled={loading}
                     >
                         {loading ? "Signing in..." : "Sign in"}
